04e242c37ce998e2c2272c1db0b3d1c10e10ee0d
SHUN5872698 <tekisasu.bager@gmail.com>
2025-08-11 00:33:33 +0900

    feat: Vue.js環境を導入
    - Vue.js 3とViteをプロジェクトに統合
    - `package.json`と`package-lock.json`にVue関連の依存関係を追加
    - `vite.config.js`を更新し、Vueプラグインを設定

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+  },
+  plugins: [
+    vue(),
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
+  ],
 });
